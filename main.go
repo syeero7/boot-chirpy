ec6aeb9cbb9999807d826ffa6bf6a55123c0a69e
emal <emalhasitha@proton.me>
2025-09-30 02:29:28 +0530

    refactor: separate server readiness check into a dedicated function

--- main.go
+++ main.go
@@ -12,16 +12,18 @@ func main() {
 	config := apiConfig{}
 	mux.Handle("/app/", http.StripPrefix("/app/", config.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
 	mux.Handle("/app/assets/logo.png", http.StripPrefix("/app/", http.FileServer(http.Dir("./assets/logo.png"))))
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(http.StatusText(http.StatusOK)))
-	})
+	mux.HandleFunc("/healthz", getServerReadiness)
 	mux.HandleFunc("/metrics", config.getRequestCount)
 	mux.HandleFunc("/reset", config.resetRequestCount)
 
 	server := &http.Server{Addr: ":8080", Handler: mux}
 	log.Fatal(server.ListenAndServe())
+}
+
+func getServerReadiness(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
 
 type apiConfig struct {
