3e0bc7ecd3da9e3c66a572ac8e2f982eef6db3ee
emal <emalhasitha@proton.me>
2025-09-29 23:41:07 +0530

    feat: add app file server and readiness routes

--- main.go
+++ main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/assets/logo.png", http.StripPrefix("/app/", http.FileServer(http.Dir("./assets/logo.png"))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(http.StatusText(http.StatusOK)))
+	})
+
+	server := &http.Server{Addr: ":8080", Handler: mux}
+	log.Fatal(server.ListenAndServe())
 }
